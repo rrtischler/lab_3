96025e7e19090dc077d9c38b7ea877422eac4784
Rafael Tischler <r.tischler@gmail.com>
2017-12-13 12:04:30 -0300

    Correção do delay DSP
    Agora está gravando todas as amostras na saída.

--- prob_1/Code/DSP/lab3_ativ1/Debug/objects.mk
+++ prob_1/Code/DSP/lab3_ativ1/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -llibc.a -l"F:/Google\ Drive/UFBA/2017.2/Lab3/docs/PBL/lab_3/Docs/Ambientacao\ com\ o\ CCS/ezdsp5502_v1/lib/ezdsp5502bsl.lib" -l"F:/Google\ Drive/UFBA/2017.2/Lab3/docs/PBL/lab_3/Docs/Ambientacao\ com\ o\ CCS/ezdsp5502_v1/C55xxCSL/lib/csl5502.lib"
+LIBS := -llibc.a -l"F:/Google\ Drive/UFBA/2017.2/Lab3/ccs_workspace/ezdsp5502_v1/lib/ezdsp5502bsl.lib" -l"F:/Google\ Drive/UFBA/2017.2/Lab3/ccs_workspace/ezdsp5502_v1/C55xxCSL/lib/csl5502.lib"
 
