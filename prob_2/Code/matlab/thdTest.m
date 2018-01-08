1d3cdc2946bddf67b348a3ba5a8149d68a9c278b
Aline Shayonara <shayramos@hotmail.com>
2018-01-08 17:33:18 -0300

    testes matlab THD

--- prob_2/Code/matlab/thdTest.m
+++ prob_2/Code/matlab/thdTest.m
@@ -1,8 +1,21 @@
 %function [  ] = ThdF()
 
-    [y, fs] = audioread('dspafsx_mono.wav');
-    [y1, fs1] = audioread('out_DSPdelay.wav');
-    r = thd(y)
-    r1 = thd(y1)
-%end
+    [y, fs] = audioread('out_DSPdelay.wav');
+    m = length(y);          % original sample length 
+    n = pow2(nextpow2(m));  % transform length
+    x = fft(y, n);          % DFT of signal
+    f = (0:n-1)*(fs/n)/10;  % frequency range
+    power = abs(x).^2/n;    % power of the DFT
+    %plot(f, power)
 
+    %plot(y);
+    %xlabel('Sample number')
+    %ylabel('Amplitude')
+
+    %plot(f(1:floor(n/2)), power(1:floor(n/2)))
+    %xlabel('Frequency')
+    %ylabel('Power')
+    plot(abs(x));
+    %periodogram(y)
+    [r,harmpow,harmfreq] = thd(y, fs)
+%end
