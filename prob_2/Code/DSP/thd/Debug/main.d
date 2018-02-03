014c56397cbc1221dfad0673aee12b113622ea32
Aline Shayonara <shayramos@hotmail.com>
2018-02-03 18:11:50 -0300

    prob2

--- prob_2/Code/DSP/thd/Debug/main.d
+++ prob_2/Code/DSP/thd/Debug/main.d
@@ -1,89 +1,93 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/include/ezdsp5502.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_std.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/DSP/thd/tistdtypes.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_time.h
-main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irq.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_err.h
-main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/dsplib/include/math.h
-main.obj: /opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/string.h
-main.obj: /home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/dsplib/include/stdio.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/include/ezdsp5502.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_std.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/DSP/thd/tistdtypes.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_time.h
+main.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irq.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_err.h
+main.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/math.h
+main.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/string.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/stdio.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/Dsplib.h
+main.obj: E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/tms320.h
 
 ../main.c: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/include/ezdsp5502.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_std.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/DSP/thd/tistdtypes.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_time.h: 
-/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irq.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_err.h: 
-/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/dsplib/include/math.h: 
-/opt/ti/ccsv7/tools/compiler/c5500_4.4.1/include/string.h: 
-/home/shay/Documents/UFBA/2017.2/Lab3/Lab3_project/git_Project/lab_3/prob_2/Code/dsplib/include/stdio.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/include/ezdsp5502.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_std.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/DSP/thd/tistdtypes.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_time.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irq.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/ezdsp5502_v1/C55xxCSL/include/csl_err.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/math.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/string.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/stdio.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/Dsplib.h: 
+E:/Shay_user/Desktop/UFBA/2017.2/Lab3/lab_3/prob_2/Code/dsplib/include/tms320.h: 
